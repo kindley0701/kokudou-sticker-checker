7b1e887e99bead868520ea88e92e1f4c36f295cb
kindley0701 <kindley07011@gmail.com>
2025-11-20 23:51:32 +0900

    チェックの非同期化

--- app/controllers/public/checks_controller.rb
+++ app/controllers/public/checks_controller.rb
@@ -1,14 +1,20 @@
 class Public::ChecksController < ApplicationController
   def create
     @sticker = Sticker.find(params[:sticker_id])
-    @check = current_user.checks.new(sticker_id: @sticker.id)
-    @check.save
-    redirect_to stickers_path
+    check = current_user.checks.new(sticker_id: @sticker.id)
+    check.save
+    respond_to do |format|
+      format.turbo_stream
+      format.html {redirect_to stickers_path}
+    end
   end
   def destroy
     @sticker = Sticker.find(params[:sticker_id])
-    @check = current_user.checks.find_by(sticker_id: @sticker.id)
-    @check.destroy
-    redirect_to stickers_path
+    check = current_user.checks.find_by(sticker_id: @sticker.id)
+    check.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to stickers_path }
+    end
   end
 end
